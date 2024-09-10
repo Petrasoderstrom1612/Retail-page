4fdf9e581802672275ec326d16c180d1cd84d1f0
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-09-10 17:26:09 +0200

    same size images and buttons next to each other, you can add and reduce items in the shopping list

--- index.js
+++ index.js
@@ -1,51 +1,63 @@
-import { shoppingItems } from "./shoppingData.js"
-const body = document.getElementById("body")
-const container = document.getElementById("container")
-const addOne = document.getElementById("addOne")
-const removeOne = document.getElementById("removeOne")
-const quantity = document.getElementById("quantity")
-const buyBtn = document.getElementById("buy-btn")
-let amount = 0
-
-addOne.addEventListener("click", function(){
-    amount ++
-    quantity.innerHTML = amount
-    if(amount > 0){
-        buyBtn.disabled = false
-    }
-})
-
-removeOne.addEventListener("click", function(){
-    if(amount !== 0){
-        amount --
-        quantity.innerHTML = amount
-    }
-    if(amount === 0){
-        buyBtn.disabled = true
-    }
-})
+import { shoppingItems } from "./shoppingData.js";
+const container = document.getElementById("container");
+let amount = 0;
 
 function renderProducts(products) {
-    let html = ""
-    for(let product of products){
-        html += `
+  let html = "";
+  for (let product of products) {
+    html += `
         <div class="product" id="${product.name}">
             <h1>${product.name.toUpperCase()}</h1>
             <h2>$${product.price}</h2>
             <img alt=${product.name} src="./${product.img}"/>
-            <p>Quantity: ${product.minShoppingAmount}</p>
-            <div class="flex"><button id="${product.name}+1">+</button><button id="${product.name}-1">-</button></div>
-            <button id="${product.id}-buy">BUY</button>
+            <div>
+                <button id="${product.name}-removeOne">-</button>
+                <button id="${product.name}-addOne">+</button>
+            </div>
+            <p>Quantity: <span id="${product.name}-quantity">0</span></p>
+            <button  id="${product.name}-btn" disabled>BUY</button>
         </div>
-        `
-    }
+        `;
+  }
 
-  container.innerHTML = html
+  container.innerHTML = html;
+  for (let product of products) {
+    addEventListeners(product);
+  }
 }
 
-renderProducts(shoppingItems)
+renderProducts(shoppingItems);
 
-container.addEventListener("click",function(e){
-    console.log(`${e.target.parentElement.id.charAt(0).toUpperCase() + e.target.parentElement.id.slice(1)} added to the shopping cart.`)
-})
+function addEventListeners(product) {
+  const addOne = document.getElementById(`${product.name}-addOne`);
+  const removeOne = document.getElementById(`${product.name}-removeOne`);
+  const quantity = document.getElementById(`${product.name}-quantity`);
+  const buyBtn = document.getElementById(`${product.name}-btn`);
 
+  addOne.addEventListener("click", function () {
+    amount++;
+    quantity.innerHTML = amount;
+    if (amount > 0) {
+      buyBtn.disabled = false;
+    }
+  });
+
+  removeOne.addEventListener("click", function () {
+    if (amount !== 0) {
+      amount--;
+      quantity.innerHTML = amount;
+    }
+    if (amount === 0) {
+      buyBtn.disabled = true;
+    }
+  });
+}
+
+container.addEventListener("click", function (e) {
+  console.log(
+    `${
+      e.target.parentElement.id.charAt(0).toUpperCase() +
+      e.target.parentElement.id.slice(1)
+    } added to the shopping cart.`
+  );
+});
