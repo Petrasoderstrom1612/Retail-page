94b781c9bf89c2b4cebb5e1d17775af8f1136e17
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2024-09-10 16:05:39 +0200

    added import of data.js to make the js file clearer

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
+import { shoppingItems } from "./shoppingData.js"
 const body = document.getElementById("body")
 const container = document.getElementById("container")
 
-const shoppingItems =[
-    {
-        name: "ostrich pillow",
-        price: 10,
-        img: "ostrichpillow.jpg",
-        id: "ostrichpillow",
-        minShoppingAmount: 1,
-    },
-    {
-        name: "bacon bandage",
-        price: 20,
-        img: "baconbandage.jpg",
-        id: "bacon-bandage",
-        minShoppingAmount: 1,
-    },
-    {
-        name: "baby mop",
-        price: 15,
-        img: "babymop.jpg",
-        id: "baby-mop",
-        minShoppingAmount: 1,
-    }
-]
 
 function renderProducts(products) {
     let html = ""
---
+++ shoppingData.js
@@ -0,0 +1,23 @@
+export const shoppingItems =[
+    {
+        name: "ostrich pillow",
+        price: 10,
+        img: "ostrichpillow.jpg",
+        id: "ostrichpillow",
+        minShoppingAmount: 1,
+    },
+    {
+        name: "bacon bandage",
+        price: 20,
+        img: "baconbandage.jpg",
+        id: "bacon-bandage",
+        minShoppingAmount: 1,
+    },
+    {
+        name: "baby mop",
+        price: 15,
+        img: "babymop.jpg",
+        id: "baby-mop",
+        minShoppingAmount: 1,
+    }
+]
